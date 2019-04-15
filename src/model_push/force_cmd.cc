10a6d74fb08f6d6e45f0067715e9b37ac720f777
Li-Wei Chi <weberchichi@gmail.com>
2019-04-14 20:44:51 -0700

    working publisher/listener

--- src/model_push/force_cmd.cc
+++ src/model_push/force_cmd.cc
@@ -1,8 +1,9 @@
 #include <gazebo/gazebo_config.h>
 #include <gazebo/transport/transport.hh>
 #include <gazebo/msgs/msgs.hh>
+#include <geometry_msgs/Wrench.h>
 #include "ros/ros.h"
-#include "std_msgs/Float64.h"
+
 
 using namespace std;
 int main(int _argc, char **_argv) {
@@ -10,17 +11,23 @@ int main(int _argc, char **_argv) {
 
   ros::NodeHandle n;
 
-  ros::Publisher pub = n.advertise<std_msgs::Float64>("force_cmd", 100);
+  ros::Publisher pub = n.advertise<geometry_msgs::Wrench>("force_cmd", 1);
 
   ros::Rate loop_rate(10);
 
   while (ros::ok()) {
-    std_msgs::Float64 msg;
+    geometry_msgs::Wrench msg;
     char x_mag[100];
     cin >> x_mag;
 
     cout << "publishing: " << x_mag << endl;
-    msg.data = atof(x_mag);
+    msg.force.x = atof(x_mag);    
+    msg.force.y = 0;
+    msg.force.z = 0;
+    msg.torque.x = 0;
+    msg.torque.y = 0;
+    msg.torque.z = 0;
+
     pub.publish(msg);
     ros::spinOnce();
     loop_rate.sleep();
--- src/model_push/model_push.cc
+++ src/model_push/model_push.cc
@@ -1,81 +1,185 @@
-#include <functional>
-#include <gazebo/gazebo.hh>
-#include <gazebo/physics/physics.hh>
-#include <gazebo/common/common.hh>
-#include <gazebo/physics/Model.hh>
-#include <gazebo/physics/Joint.hh>
-#include <gazebo/physics/Link.hh>
-#include <gazebo/transport/transport.hh>
-#include <gazebo/msgs/msgs.hh>
-#include "ros/ros.h"
-#include "std_msgs/Float64.h"
+/*Copyright 2013 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
 
-using namespace std;
+/*
+   Desc: GazeboRosForce plugin for manipulating objects in Gazebo
+   Author: John Hsu
+   Date: 24 Sept 2008
+ */
 
+#include <algorithm>
+#include <assert.h>
+
+#include "model_push.h"
 namespace gazebo
 {
-  class ModelPush : public ModelPlugin
+GZ_REGISTER_MODEL_PLUGIN(GazeboRosForce);
+
+////////////////////////////////////////////////////////////////////////////////
+// Constructor
+GazeboRosForce::GazeboRosForce()
+{
+  this->wrench_msg_.force.x = 0;
+  this->wrench_msg_.force.y = 0;
+  this->wrench_msg_.force.z = 0;
+  this->wrench_msg_.torque.x = 0;
+  this->wrench_msg_.torque.y = 0;
+  this->wrench_msg_.torque.z = 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Destructor
+GazeboRosForce::~GazeboRosForce()
+{
+  this->update_connection_.reset();
+
+  // Custom Callback Queue
+  this->queue_.clear();
+  this->queue_.disable();
+  this->rosnode_->shutdown();
+  this->callback_queue_thread_.join();
+
+  delete this->rosnode_;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Load the controller
+void GazeboRosForce::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
+{
+  // Get the world name.
+  this->world_ = _model->GetWorld();
+
+  // load parameters
+  this->robot_namespace_ = "";
+  if (_sdf->HasElement("robotNamespace"))
+    this->robot_namespace_ = _sdf->GetElement("robotNamespace")->Get<std::string>() + "/";
+
+  if (!_sdf->HasElement("bodyName"))
   {
-    public: void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
-    {
-      // Store the pointer to the model
-      this->model = _parent;
+    ROS_FATAL_NAMED("force", "force plugin missing <bodyName>, cannot proceed");
+    return;
+  }
+  else
+    this->link_name_ = _sdf->GetElement("bodyName")->Get<std::string>();
 
-      double x_mag = 0;
-      if (_sdf->HasElement("force"))
-        x_mag = _sdf->Get<double>("force");
-      
-      this->setForce(x_mag);
-      // Listen to the update event. This event is broadcast every
-      // simulation iteration.
-      //this->updateConnection = event::Events::ConnectWorldUpdateBegin(
-      //    std::bind(&ModelPush::OnUpdate, this, mag_x));
-      this->sub = this->n.subscribe("force_cmd", 10, &ModelPush::onForceMsg, this);
-      ros::MultiThreadedSpinner spinner(2);
-    }
+  this->link_ = _model->GetLink(this->link_name_);
+  if (!this->link_)
+  {
+    ROS_FATAL_NAMED("force", "gazebo_ros_force plugin error: link named: %s does not exist\n",this->link_name_.c_str());
+    return;
+  }
 
-    // Called by the world update start event
-    /*public: void OnUpdate(double mag_x)
-    {
-      // Apply a small linear velocity to the model.
-      physics::LinkPtr body = this->model->GetLink("link");
-      math::Vector3 forceVec(mag_x, 0, 0);      
-      math::Vector3 relVec(-0.5, 0, 0);
-      body->AddForceAtRelativePosition(forceVec, relVec);
-    }*/
+  if (!_sdf->HasElement("topicName"))
+  {
+    ROS_FATAL_NAMED("force", "force plugin missing <topicName>, cannot proceed");
+    return;
+  }
+  else
+    this->topic_name_ = _sdf->GetElement("topicName")->Get<std::string>();
 
-    private: void onForceMsg(const std_msgs::Float64::ConstPtr& msg) {
-      double x_mag = msg->data;
-      std::ostringstream ss;
-      ss << msg->data;
-      std::string s(ss.str());
-      cout << "received: " << s << endl;
-      this->setForce(msg->data);
-    }
+  std::cout << "robotNamespace: " << this->robot_namespace_ << ", " <<  
+      "subscribing to topic: " << this->topic_name_ << ", " << 
+      "applying to link: " << this->link_name_ << std::endl;
+  // Make sure the ROS node for Gazebo has already been initialized
+  if (!ros::isInitialized())
+  {
+    ROS_FATAL_STREAM_NAMED("force", "A ROS node for Gazebo has not been initialized, unable to load plugin. "
+      << "Load the Gazebo system plugin 'libgazebo_ros_api_plugin.so' in the gazebo_ros package)");
+    return;
+  }
 
-    private: void setForce(const double &x_mag) {
-      // Apply a small linear velocity to the model.
-      cout << "setting force." << endl;
-      std::ostringstream ss;
-      ss << x_mag;
-      std::string s(ss.str());
-      cout << s << endl;
+  this->rosnode_ = new ros::NodeHandle(this->robot_namespace_);
+  std::cout << "ROS NODE initialized" << std::endl;
+  // Custom Callback Queue
+  
+ros::SubscribeOptions so = ros::SubscribeOptions::create<geometry_msgs::Wrench>(
+    this->topic_name_,1,
+    boost::bind( &GazeboRosForce::UpdateObjectForce,this,_1),
+    ros::VoidPtr(), &this->queue_);
+  this->sub_ = this->rosnode_->subscribe(so);
 
-      physics::LinkPtr body = this->model->GetLink("link");
-      math::Vector3 forceVec(x_mag, 0, 0);      
-      math::Vector3 relVec(-0.5, 0, 0);
-      body->AddForceAtRelativePosition(forceVec, relVec);
-    }
-    // Pointer to the model
-    private: physics::ModelPtr model;
+  // Custom Callback Queue
+  this->callback_queue_thread_ = boost::thread( boost::bind( &GazeboRosForce::QueueThread,this ) );
 
-    // Pointer to the update event connection
-    private: event::ConnectionPtr updateConnection;
-    private: ros::NodeHandle n;
-    private: ros::Subscriber sub;  
-  };
 
-  // Register this plugin with the simulator
-  GZ_REGISTER_MODEL_PLUGIN(ModelPush)
+  // New Mechanism for Updating every World Cycle
+  // Listen to the update event. This event is broadcast every
+  // simulation iteration.
+  this->update_connection_ = event::Events::ConnectWorldUpdateBegin(
+      boost::bind(&GazeboRosForce::UpdateChild, this));
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Update the controller
+void GazeboRosForce::UpdateObjectForce(const geometry_msgs::Wrench::ConstPtr& _msg)
+{
+  std::cout << "updating object force.." << std::endl;
+  this->wrench_msg_.force.x = _msg->force.x;
+  this->wrench_msg_.force.y = _msg->force.y;
+  this->wrench_msg_.force.z = _msg->force.z;
+  this->wrench_msg_.torque.x = _msg->torque.x;
+  this->wrench_msg_.torque.y = _msg->torque.y;
+  this->wrench_msg_.torque.z = _msg->torque.z;
+
+  // reset forces to 0 after 1 second
+  // this->timer = this->rosnode_->createTimer(ros::Duration(1), boost::bind(&GazeboRosForce::ResetForce, this, _1), true);
+}
+
+void GazeboRosForce::ResetForce(const ros::TimerEvent& event) {
+  std::cout << "reseting forces.." << std::endl;
+  ignition::math::Vector3d force(-this->wrench_msg_.force.x,-this->wrench_msg_.force.y,-this->wrench_msg_.force.z);
+ 
+  ignition::math::Vector3d relVec(0.5, 0, 0);
+  this->link_->AddForceAtRelativePosition(force, relVec);
+
+  this->wrench_msg_.force.x = 0;
+  this->wrench_msg_.force.y = 0;
+  this->wrench_msg_.force.z = 0;
+  this->wrench_msg_.torque.x = 0;
+  this->wrench_msg_.torque.y = 0;
+  this->wrench_msg_.torque.z = 0;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Update the controller
+void GazeboRosForce::UpdateChild()
+{
+  this->lock_.lock();
+  ignition::math::Vector3d force(this->wrench_msg_.force.x,this->wrench_msg_.force.y,this->wrench_msg_.force.z);
+  ignition::math::Vector3d torque(this->wrench_msg_.torque.x,this->wrench_msg_.torque.y,this->wrench_msg_.torque.z);
+  ignition::math::Vector3d relVec(-0.5, 0, 0);
+  this->link_->AddForceAtRelativePosition(force, relVec);
+  this->link_->AddTorque(torque);
+  this->lock_.unlock();
+}
+
+// Custom Callback Queue
+////////////////////////////////////////////////////////////////////////////////
+// custom callback queue thread
+
+void GazeboRosForce::QueueThread()
+{
+  static const double timeout = 0.01;
+
+  while (this->rosnode_->ok())
+  {
+    this->queue_.callAvailable(ros::WallDuration(timeout));
+  }
+}
+
 }
 
