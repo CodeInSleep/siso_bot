053ea272b2f2ad13c8be7e44232507c768ca49a4
Li-Wei Chi <weberchichi@gmail.com>
2019-04-22 16:52:51 -0700

    create data dir if does not exist

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@ export GAZEBO_PLUGIN_PATH=$PWD/src/model_push/nodes:$GAZEBO_PLUGIN_PATH
 source devel/setup.bash
 # for writing one data file
 export DATA_DIR = "/Users/li-wei/siso_bot/data/"
+mkdir data
