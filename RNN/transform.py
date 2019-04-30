9a6ac40089df96675218bdfe5c5b8ca7aec8419e
Li-Wei Chi <weberchichi@gmail.com>
2019-04-29 19:07:09 -0700

    add shuffling to pick out train/test trials

--- RNN/transform.py
+++ RNN/transform.py
@@ -31,6 +31,9 @@ def transform_group(group_df, max_duration, output_fields):
     # pad the time series with 
     padded_group_df = pd.DataFrame(pd.np.row_stack([group_df, padding]))
     padded_group_df.columns = cols 
+    padded_group_df.loc[:, output_fields] = padded_group_df.loc[:, output_fields].diff()
+    padded_group_df = padded_group_df.fillna(0)
+
     return padded_group_df
 
 def transform(df, input_fields, output_fields, train_percentage=0.7, count=-1):
@@ -52,7 +55,7 @@ def transform(df, input_fields, output_fields, train_percentage=0.7, count=-1):
 
     # store max duration of a trial
     max_duration = max(grouped['sim_time'].count())
-
+    n_train = int(num_trials*train_percentage)
 
     # the start time of every trial, used later to recover trajectories
     start_times = grouped.first()
@@ -61,19 +64,25 @@ def transform(df, input_fields, output_fields, train_percentage=0.7, count=-1):
     # remove the bias of starting points in each trial
     df.loc[:, output_fields] = grouped.apply(
         lambda x: x.loc[:, output_fields] - start_times.loc[x.name].loc[output_fields])
-
-    # normalize x, y, theta outputs
-    df.loc[:, output_fields] = output_scaler.fit_transform(df.loc[:, output_fields])
-
     # create new data frame that is of (# of trials, max_duration dimenstion) 
     df = df.groupby(['input']).apply(lambda x: transform_group(x, max_duration, output_fields))
 
-    # unstack time series to columns
-    df = df.unstack(level=1)
+    trial_names = df.index.levels[0]
+    train_samples = np.random.choice(num_trials, n_train, replace=False)
+    train_trial_names = [trial_names[i] for i in train_samples]
+    test_samples = np.array([i for i in range(num_trials) if i not in train_samples])
+    test_trial_names = [trial_names[i] for i in test_samples]
 
-    n_train = int(num_trials*train_percentage)
-    train_data = df.iloc[:n_train, :]
-    test_data = df.iloc[n_train:, :]
+    train_data = df.loc[train_trial_names, :]
+    test_data = df.loc[test_trial_names, :] 
+
+    # normalize the output differences
+    train_data.loc[:, output_fields] = output_scaler.fit_transform(train_data.loc[:, output_fields])
+    test_data.loc[:, output_fields] = output_scaler.transform(test_data.loc[:, output_fields])
+
+    # unstack time series to columns
+    train_data = train_data.unstack(level=1)
+    test_data = test_data.unstack(level=1)
 
     p = len(input_fields)
     J = len(output_fields)
@@ -85,6 +94,7 @@ def transform(df, input_fields, output_fields, train_percentage=0.7, count=-1):
     X_test = test_data[input_fields].values.reshape(num_trials-n_train, p, max_duration).transpose(0, 2, 1)
     y_train = train_data[output_fields].values.reshape(n_train, J, max_duration).transpose(0, 2, 1)
     y_test = test_data[output_fields].values.reshape(num_trials-n_train, J, max_duration).transpose(0, 2, 1)
+
     # access trial of specific inputs with df.loc['INPUT_VALUES', :]
     return (X_train, X_test, y_train, y_test, train_trial_names, test_trial_names, output_scaler, start_times, max_duration)
 
