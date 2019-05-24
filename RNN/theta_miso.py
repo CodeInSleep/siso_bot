5863f10cbec6be203a3449ed6dd12239718beca6
Li-Wei Chi <weberchichi@gmail.com>
2019-05-24 16:01:18 -0700

    reorganize code

--- RNN/theta_miso.py
+++ RNN/theta_miso.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 
 from transform import transform
 from visualize import visualize_3D
-from utils import decode_angles, plot_target_angles, save_obj, make_model, save_model, angle_dist
+from utils import decode_angles, plot_target_angles, save_obj, save_model, angle_dist, make_model, load_obj
 
 input_fields = ['left_pwm', 'right_pwm']
 
@@ -41,23 +41,29 @@ if __name__ == '__main__':
     print('dirpath: ', dirpath)
     datafile = os.path.join(dirpath, fname)
     df = pd.read_csv(datafile, engine='python')
+    if fname == 'trial_2000_0_to_3':
+        df = df.drop(['blank1', 'blank2'], axis=1)
+
 
     p = layers_dims[0]
     J = layers_dims[-1]
-    X_train, X_test, theta_y_train, _, theta_y_test, _, timestep = transform(df, layers_dims, dirpath, cached=False)
-    X_train = X_train.values.reshape(
+    X_train, X_test, y_train, y_test, timestep = transform(df, layers_dims, dirpath, cached=True)
+    X_train = X_train.loc[:, ['sim_time', 'left_pwm', 'right_pwm', 'theta(t-1)_cos', 'theta(t-1)_sin']].values.reshape(
         -1, timestep, p)
-    X_test = X_test.values.reshape(
+    X_test = X_test.loc[:, ['sim_time', 'left_pwm', 'right_pwm', 'theta(t-1)_cos', 'theta(t-1)_sin']].values.reshape(
             -1, timestep, p)
-    theta_y_train = theta_y_train.values.reshape(
+    theta_y_train = y_train.loc[:, ['theta_cos', 'theta_sin']].values.reshape(
             -1, timestep, J)
-    theta_y_test = theta_y_test.values.reshape(
+    theta_y_test = y_test.loc[:, ['theta_cos', 'theta_sin']].values.reshape(
                 -1, timestep, J)
+    # pdb.set_trace()
 
     num_batches = 16
-    theta_model = make_model(num_batches, timestep, layers_dims)
+    theta_model = make_model(num_batches, timestep, layers_dims, lr=1e-4)
+    train_trial_names = load_obj(dirpath, 'train_trial_names')
+    test_trial_names = load_obj(dirpath, 'test_trial_names')
    
-    iterations = 10
+    iterations = 3
     epochs = 30
     # learning curver
     train_loss_history = []
@@ -68,28 +74,9 @@ if __name__ == '__main__':
     for it in range(iterations):
         print("iteration %d" % it)
         theta_model.fit(X_train, theta_y_train, epochs=epochs, batch_size=num_batches, verbose=1, shuffle=False)
-        # for j in range(int(train_bl/time_step)-1):
-        #     time_seg = range((j*time_step), ((j+1)*time_step))
-        #     model.fit(X_train[:, time_seg, :], y_train[:, time_seg, :], 
-        #             batch_size=num_batches, verbose=1, shuffle=False)
-        # model.reset_states()
 
         # calculate rmse for train data
         train_se = []
-        # for j in range(int(train_bl/time_step)-1):
-        #     time_seg = range((j*time_step), ((j+1)*time_step))
-        #     pred = model.predict(X_train[:, time_seg, :])
-
-        #     train_se_over_timestep = []
-        #     for k in range(num_batches):
-        #         _pred = decode_angles(pred[k])
-        #         _gnd_truth = decode_angles(y_train[k, time_seg, :])
-                
-        #         diff = np.apply_along_axis(angle_dist, 1,
-        #                 np.concatenate((_pred, _gnd_truth), axis=1))
-        #         train_se_over_timestep.append(diff**2)
-        #     train_se.append(train_se_over_timestep)
-        # model.reset_states()
         train_predictions = theta_model.predict(X_train)
         for i in range(len(train_predictions)):
             pred = decode_angles(train_predictions[i])
@@ -103,20 +90,6 @@ if __name__ == '__main__':
         train_loss_history.append(train_rmse)
         
         test_se = []
-        # for j in range(int(test_bl/time_step)-1):
-        #     time_seg = range((j*time_step), ((j+1)*time_step))
-        #     pred = model.predict(X_test[:, time_seg, :])
-            
-        #     test_se_over_timestep = []
-        #     for k in range(num_batches):
-        #         _pred = decode_angles(pred[k])
-        #         _gnd_truth = decode_angles(y_test[k, time_seg, :])
-        #         diff = np.apply_along_axis(angle_dist, 1,
-        #                 np.concatenate((_pred, _gnd_truth),
-        #                     axis=1))
-        #         test_se_over_timestep.append(diff**2)
-        #     test_se.append(test_se_over_timestep)
-        # model.reset_states()
         test_predictions = theta_model.predict(X_test)
         for i in range(len(X_test)):
             pred = decode_angles(test_predictions[i])
@@ -129,9 +102,13 @@ if __name__ == '__main__':
             plt.cla()
             plt.plot(pred)
             plt.plot(gnd_truth)
+            plt.title('theta miso predictions (Iteration {})'.format(it+1))
+            plt.text(0.9, 0.9, test_trial_names[i])
+            plt.ylabel('radians')
+            plt.xlabel('timstep (1/0.2s)')
             plt.legend(['pred', 'ground truth'])
             plt.grid(True)
-            plt.pause(0.000001)
+            plt.pause(0.001)
 
         test_rmse = np.sqrt(np.mean(np.array(test_se)))
         test_loss_history.append(test_rmse)
@@ -142,13 +119,13 @@ if __name__ == '__main__':
     model_fname = 'theta_model'
     save_model(theta_model, dirpath, model_fname)
 
-    error_plot = plt.figure()
+    fig = plt.figure()
     ep_range = range(0, iterations)
     plt.plot(ep_range, train_loss_history)
     plt.plot(ep_range, test_loss_history)
     plt.title('theta miso prediction (RNN)')
-    plt.xlabel('epoch')
+    plt.xlabel('iteration (30 epochs/iteration)')
     plt.ylabel('RMSE theta(rad)')
-    error_plot.show()
+    fig.show()
 
     
